d2bb02b6cadc993fd5fe83278fa27127ec39fc65
maxmarumbwa <maxmarumbwa@gmail.com>
2025-05-10 21:37:46 +0200

    serialiser comments

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,3 +1,5 @@
+# Serializers are used to convert complex data types, like querysets and model instances, into native Python datatypes.
+# They also handle deserialization, converting parsed data back into complex types.
 from rest_framework import serializers
 from .models import Course
 
